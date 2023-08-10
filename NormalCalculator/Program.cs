63579ab14ca431d6f81b070d9dc190d18ea36b56
Arsenii <arsenii.rykov@gmail.com>
2023-08-10 12:32:24 +0300

    Добавил key input

--- NormalCalculator/Program.cs
+++ NormalCalculator/Program.cs
@@ -9,19 +9,30 @@ namespace NormalCalculator
 {
   internal class Program
   {
+    long currentNumber = 0;
+    long buffer = 0;
+
     static void Main(string[] args)
     {
-      DrawCalculatorLayout(1234567890, "");
+      DrawCalculatorLayout(0, "");
+      while (true)
+      {
+        Console.SetCursorPosition(0, 0);
+        string keyPressed = GetKey();
+
+        DrawCalculatorLayout(1234567890, keyPressed);
+      }
       Console.ReadKey();
     }
-    static string[,] keys = new string[5, 4]
+
+    static string[,] keys = new string[4, 5]
         {
-          {" - ", " / ", "   ", "   " },
-          {" + ", " * ", " = ", "   "},
-          {" 9 ", " 6 ", " 3 ", "-/+"},
-          {" 8 ", " 5 ", " 2 ", " 0 "},
-          {" 7 ", " 4 ", " 1 ", "   "},
+          {" 7 ", " 8 ", " 9 ", " + ", " - " },
+          {" 4 ", " 5 ", " 6 ", " * ", " / " },
+          {" 1 ", " 2 ", " 3 ", " = ", "   " },
+          {"   ", " 0 ", "   ", "   ", " C " },
         };
+
     static void DrawCalculatorLayout(int currentNumber, string selectedKey)
     {
       //-------------------------------
@@ -33,16 +44,27 @@ namespace NormalCalculator
       //-------------------------------
       //|  1  |  2  |  3  |  =  |     |
       //-------------------------------
-      //|     |  0  | -/+ |           |
+      //|     |  0  | -/+ |     |  C  |
       //-------------------------------
       //               ^
       //               |
-      //            Press.
+      //            Press .
       string horizontalWall = new string('-', 31);
+      if (selectedKey == ".")
+      {
+        selectedKey = "-/+";
+      }
+      if (selectedKey == "c")
+      {
+        selectedKey = "C";
+      }
       if (selectedKey.Length == 1)
       {
         selectedKey = " " + selectedKey + " ";
       }
+
+
+      Console.SetCursorPosition(0, 0);
       for (int i = 1; i <= 6; i++)
       {
         Console.WriteLine(horizontalWall);
@@ -51,15 +73,15 @@ namespace NormalCalculator
       for (int i = 0; i < 4; i++)
       {
         Console.SetCursorPosition(0, 3 + i * 2);
-        for (int j = 4; j >= 0; j--)
+        for (int j = 0; j < 5; j++)
         {
           Console.Write("|");
-          if (selectedKey == keys[j, i])
+          if (selectedKey == keys[i, j])
           {
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.ForegroundColor = ConsoleColor.Black;
           }
-          Console.Write(" " + keys[j, i] + " ");
+          Console.Write(" " + keys[i, j] + " ");
           Console.ResetColor();
         }
         Console.Write("|");
@@ -73,7 +95,12 @@ namespace NormalCalculator
       Console.SetCursorPosition(15, 12);
       Console.Write("|");
       Console.SetCursorPosition(12, 13);
-      Console.Write("Press .");
-    }                               
+      Console.WriteLine("Press .");
+    }    
+    
+    static string GetKey()
+    {
+      return Console.ReadKey().KeyChar.ToString();
+    }
   }                              
 }
